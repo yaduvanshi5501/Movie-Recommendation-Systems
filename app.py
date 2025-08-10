9d93d050cf3afda22b0f0ac1761d086a609bb355
yaduvanshi5501 <satish.kumar.iem@gmail.com>
2025-08-10 14:49:00 +0530

    Added gdown file download support for Streamlit Cloud

--- app.py
+++ app.py
@@ -1,60 +1,38 @@
+import os
+import gdown
 import pickle
 import streamlit as st
 import requests
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+# Download function
+def download_from_gdrive(file_id, destination):
+    if not os.path.exists(destination):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        gdown.download(url, destination, quiet=False)
 
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+# Ensure Model folder exists
+os.makedirs("Model", exist_ok=True)
 
-    return recommended_movie_names,recommended_movie_posters
+# File IDs (replace the second one with your similarity.pkl ID)
+movie_list_file_id = "1jbRbB9YB-L-__t57iKRaWevUTSsgSigC"
+similarity_file_id = "YOUR_SIMILARITY_FILE_ID"
 
+# Download files if missing
+download_from_gdrive(movie_list_file_id, "Model/movie_list.pkl")
+download_from_gdrive(similarity_file_id, "Model/similarity.pkl")
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('Model\movie_list.pkl','rb'))
-similarity = pickle.load(open('Model\similarity.pkl','rb'))
+# Load models with caching
+@st.cache_resource
+def load_movie_list():
+    with open("Model/movie_list.pkl", "rb") as f:
+        return pickle.load(f)
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+@st.cache_resource
+def load_similarity():
+    with open("Model/similarity.pkl", "rb") as f:
+        return pickle.load(f)
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+movies = load_movie_list()
+similarity = load_similarity()
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
-
-
-
+# Your existing code (fetch_poster, recommend, etc.)
